5f46e356d82978e50cefeb764b9c0917cceb7e24
unknown <kulangana@yandex.ru git config --global user.name Kulangana-ddgit --global user.email kulangana@yandex.ru>
2023-01-02 17:28:48 +0300

    в файлы с замерами импортированы функции

--- zamer1.py
+++ zamer1.py
@@ -1,12 +1,12 @@
 from timeit import timeit
-
-print(timeit("func_1(23456785432)", setup="from task1 import func_1", number=100000))
-print(timeit("func_2(23456785432)", setup="from task1 import func_2", number=100000))
-
-'''Всегда разные результаты. В основном ускорения не наблюдается.'''
-
-
-# Почему-то не работает через globals
+from task1 import func_1
+from task1 import func_2
 
 print(timeit("func_1(23456785432)", globals=globals(), number=100000))
 print(timeit("func_2(23456785432)", globals=globals(), number=100000))
+
+'''Всегда разные результаты. В основном ускорения не наблюдается'''
+
+# вариант без импортирования функции
+#print(timeit("func_1(23456785432)", setup="from task1 import func_1", number=100000))
+#print(timeit("func_2(23456785432)", setup="from task1 import func_2", number=100000))
--- zamer2.py
+++ zamer2.py
@@ -1,6 +1,14 @@
 from timeit import timeit
+from task2 import func_1
+from task2 import func_2
 
-print(timeit("func_1(x, y)", setup="from task2 import func_1, x, y", number=100000))
-print(timeit("func_2(x, y)", setup="from task2 import func_2, x, y", number=100000))
+print(timeit("func_1(333, 2)", globals=globals(), number=100000))
+print(timeit("func_2(333, 2)", globals=globals(), number=100000))
 
-'''Не добились ускорения процесса решения'''
+'''Всегда разные результаты. В основном ускорения не наблюдается'''
+
+
+# вариант без импортирования функции
+# print(timeit("func_1(x, y)", setup="from task2 import func_1, x, y", number=100000))
+# print(timeit("func_2(x, y)", setup="from task2 import func_2, x, y", number=100000))
+
